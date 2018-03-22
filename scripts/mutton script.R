daa4d7c1154de40b82d67259d24f2fa506fa561b
Kim Newell <Kim.Newell@csiro.au>
2018-03-22 14:44:05 +1100

    I will come back to this later

--- scripts/mutton script.R
+++ scripts/mutton script.R
@@ -152,5 +152,6 @@ ggplot(data = dfthree, aes(x = C17.0, y = measurement, color = other_fa)) +
 ggplot(data = dfthree, aes(x = C17.0, 
     y = measurement, color = other_fa)) +
   geom_point(aes(shape = factor(Year))) + geom_smooth()
+#Need to fininsh this off later
 
 
